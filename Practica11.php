67316d716e773fcbb7ccc7acbb25c2fa369218bc
Paola Alvarez <123213733+AlvarezPaola16@users.noreply.github.com>
2024-03-07 20:46:34 -0600

    Add files via upload

---
+++ Practica11.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="keywords" content="">
+        <meta name="description" content="">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <!-- navbar -->
+        <?php  include "includes/NavBar.php"; ?>
+        <?php  include "includes/Reg.php"; ?>
+        <?php  include "includes/Footer.php"; ?>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
