9995524a13d9da458adc998d945d425c3958dd48
Paola Alvarez <123213733+AlvarezPaola16@users.noreply.github.com>
2024-03-07 20:41:54 -0600

    Add files via upload

---
+++ Practica6.java
@@ -0,0 +1,20 @@
+ function validar(){
+    var usuario = Get Elemtby id("CAJA2")value;
+    var usuario = Get Elemtby id("CAJA4")value;
+    var User="asdf"
+    
+    if(usuario ==""){
+        alert("error el campo usuario vacio");
+    }
+    elseif(contra ==""){
+        alert("error el campo password vacio");
+
+    }
+    else{(usuario==User and contra==pass){
+            locatino(inicio.html);
+        }
+    }
+    else{
+        alert("user o pass incorrecto")
+    }
+ }
