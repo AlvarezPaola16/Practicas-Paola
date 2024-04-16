0464c867a5f67e317050d3bc9bb00b0b4fa5a15d
Paola Alvarez <123213733+AlvarezPaola16@users.noreply.github.com>
2024-04-15 18:06:26 -0600

    Add files via upload

---
+++ 12 practica.php
@@ -0,0 +1,93 @@
+
+
+ <section id="Nosotros">
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-6">
+        <img src="img/digitalcherry1.png" class="img-fluid" alt="">
+      </div>
+      <div class="col-sm-6 col-md-6 col-lg-6">
+        <div class="row mt-5 py-3">
+
+          <h2 class="text-secondary display-6 text-center text-danger font"
+            style="font-family: 'Times New Roman', Times, serif; color: purple;">
+
+            DISEÑAMOS APP INTUITIVAS
+          </h2>
+
+          <span class="text-center text-danger" style="font-family: Georgia, 'Times New Roman', Times, serif;">DIGITAL
+            CHERRY</span>
+
+          <p class="text-secondary text-center"
+            style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+            Somos una empresa que se dedica al desarrollo de softwar te invitamos a conocer mas de nosotros
+            pues aseguramos que escogernos es la mejor opcion , puedes encontar una gama de servicios para ti ¡animate
+            ya¡ </p>
+
+          <p class="text-secondary text-center"
+            style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">¿TE INTERESA SABER MAS DE
+            NOSOTROS? </p>
+          <p class="text-secondary text-center" style="font-family: Verdana, Geneva, Tahoma, sans-serif;">Te invitamos a
+            seguir conociendonos </p>
+
+        </div>
+      </div>
+    </div>
+  </section>
+  <section id="servicios">
+    <div class="container">
+      <div class="row mt-3">
+        <div class="col-sm-12 col-md-6 col-lg-3 mb-2" data-aos="fade-up">
+          <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+              style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+              Mision</h4>
+            <div class="row mt-2">
+              <p class="text-center" style="font-family: 'Times New Roman', Times, serif;">Digital Cherry, tiene la
+                misión de ser la empresa líder en el mercado de la tecnología por su competitividad y productividad de
+                nuestros servicios.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 mb-2" data-aos="fade-up">
+          <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+              style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+              Vision</h4>
+            <div class="row mt-2">
+              <p class="text-center" style="font-family: 'Times New Roman', Times, serif;">
+                Ser una empresa capaz de cumplir aquellos sueños que surgen a través de las necesidades por medio de la
+                tecnología con las herramientas necesarias para satisfacer y cubrir aquellas necesidades.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 mb-2" data-aos="fade-up">
+          <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+              style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+              Politica de Calidad </h4>
+            <div class="row mt-2">
+              <p class="text-center" style="font-family: 'Times New Roman', Times, serif;">En Digital Cherry buscamos
+                asegurar la confidencialidad, integridad y disponibilidad , nos concentramos en las necesidades y
+                expectativas de cada una de las partes interesadas.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-3 col-lg-3 mb-2" data-aos="fade-up">
+          <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+              style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">
+              Servicios</h4>
+            <div class="row mt-2">
+              <p class="text-center">Ofrecemos diferentes servicios entre ellos, </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
